df2aa65c66a23f58c06ad373c3ab9fa444366023
martinezmanuel8 <martinezmanuel.8@gmail.com>
2025-06-23 15:09:29 +0200

    Se agrega la funcionalidad de ver producto en la pagina de productohome

---
+++ src/main/java/com/ecomerce/controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.ecomerce.controller;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecomerce.model.Producto;
+import com.ecomerce.service.ProductoService;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	@GetMapping("")
+	public String home(Model model) {
+		model.addAttribute("productos", productoService.findAll());
+		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {		
+		LOGGER.info("Id producto enviado como parametro {}",id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+
+		model.addAttribute("producto", producto);
+		return "usuario/productohome";
+	}	
+
+}
--- src/main/java/com/ecomerce/controller/ProductoController.java
+++ src/main/java/com/ecomerce/controller/ProductoController.java
@@ -74,21 +74,21 @@ public class ProductoController {
 	
 	@PostMapping("update")
 	public String update(Producto producto,  @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p= productoService.get(producto.getId()).get();
+		
 		if(file.isEmpty()) {// opcion para cuando editamos el producto pero no cambiamos la imagen
-			Producto p = new Producto();
-			p= productoService.get(producto.getId()).get();
 			producto.setImagen(p.getImagen());
 		}else {//cuando tambien se edita la imagen
-			Producto p = new Producto();
-			p= productoService.get(producto.getId()).get();
-			
-			if(!p.getImagen().equals("default.jpg"))
-			{
-				upload.deleteImage(p.getImagen());
+			//se deben validar que no se eliman la imagen por defecto
+			if(p.getImagen() != null && !p.getImagen().equals("default.jpg")) {				 
+					upload.deleteImage(p.getImagen());				 
 			}
+			
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);				
 		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		LOGGER.info("PRODUCTO ACTUALIZADO:{}", producto); 
 		return "redirect:/productos";
